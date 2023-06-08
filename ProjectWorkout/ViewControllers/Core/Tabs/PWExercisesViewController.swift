4d9e451616f7e9789d95c0599db235ac0189d0a9
PremyslM <premyslmikulenka0@gmail.com>
2023-06-08 14:08:23 +0200

    Added iconImage to each actvity and axercise.

--- ProjectWorkout/ViewControllers/Core/Tabs/PWExercisesViewController.swift
+++ ProjectWorkout/ViewControllers/Core/Tabs/PWExercisesViewController.swift
@@ -55,7 +55,8 @@ extension PWExercisesViewController: UICollectionViewDelegate, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CellIndentifiers.exercise.rawValue, for: indexPath) as? PWPrimaryCollectionViewCell else { fatalError() }
-        cell.titleLabel.text = vc.unwrappedExercises[indexPath.row].name  
+        cell.titleLabel.text = vc.unwrappedExercises[indexPath.row].name
+        cell.iconImage.image = UIImage(systemName: "xmark")
         return cell
     }
     
