ca70e459de55c8942223553e16565c5a0753c6b1
PremyslM <premyslmikulenka0@gmail.com>
2023-07-05 18:25:04 +0200

    Added font and position changes in our info training view

--- ProjectWorkout/Views/PWTrainingInfoCardView.swift
+++ ProjectWorkout/Views/PWTrainingInfoCardView.swift
@@ -11,25 +11,46 @@ import UIKit
 class PWTrainingInfoCardView: PWProgrammaticUIView {
     
     let exercises = DataManager.getActivities //PWExerciseViewViewModel().unwrappedExercises
-    
+        
     private lazy var exercisesListLabel: UILabel = UILabel()
-    private lazy var difficultyLabel: UILabel = UILabel()
+        
+    private lazy var difficultyLabel: UILabel = {
+        let _difficultyLabel: UILabel = UILabel()
+        
+        _difficultyLabel.text = "Dificulty"
+        _difficultyLabel.font = .systemFont(ofSize: 10, weight: .thin)
+        
+        return _difficultyLabel
+    }()
+    
+    private lazy var exercisesLabel: UILabel = {
+        let _exercisesLabel: UILabel = UILabel()
+        
+        _exercisesLabel.text = "Exercises"
+        _exercisesLabel.font = .systemFont(ofSize: 10, weight: .thin)
+        
+        return _exercisesLabel
+    }()
+    
     
     override func setConfig() {
         self.backgroundColor = .gray
         
         exercisesListLabel.numberOfLines = 3
         
-        self.addConstrainedSubViews(exercisesListLabel, difficultyLabel)
+        self.addConstrainedSubViews(exercisesListLabel, difficultyLabel, exercisesLabel)
     }
     
     override func setConstraints() {
         NSLayoutConstraint.activate([
-            exercisesListLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
-            exercisesListLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+            exercisesLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
+            exercisesLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+            
+            exercisesListLabel.topAnchor.constraint(equalTo: exercisesLabel.bottomAnchor, constant: 3),
+            exercisesListLabel.leadingAnchor.constraint(equalTo: exercisesLabel.leadingAnchor, constant: 10),
             
             difficultyLabel.topAnchor.constraint(equalTo: exercisesListLabel.bottomAnchor, constant: 10),
-            difficultyLabel.leadingAnchor.constraint(equalTo: exercisesListLabel.leadingAnchor),
+            difficultyLabel.leadingAnchor.constraint(equalTo: exercisesLabel.leadingAnchor),
         ])
     }
     
@@ -43,8 +64,8 @@ class PWTrainingInfoCardView: PWProgrammaticUIView {
             exerciseListString += "\(exercise.name ?? "[X]")\n"
         }
         
-        exercisesListLabel.text = "Exercises:\n\(exerciseListString)"
-        difficultyLabel.text = "Difficulty: \(difficulty)"
+        exercisesListLabel.text = "\(exerciseListString)"
+        exercisesListLabel.font = .systemFont(ofSize: 15)
     }
     
     
