0cec27514e0a770a32fa372b990020a34cbfed0d
PremyslM <premyslmikulenka0@gmail.com>
2023-08-05 11:36:49 +0200

    Added comments

--- ProjectWorkout/Managers/TrainingManager.swift
+++ ProjectWorkout/Managers/TrainingManager.swift
@@ -12,7 +12,7 @@ class TrainingManager {
     public var trainingArray: [Training] {
         var result: [Training] = []
         for tIndex in 0..<5 {
-            let newTraining: Training = Training(name: "Test Trainig \(tIndex)", type: "Body Weight", duration: 45, difficulty: "Beginner", focusAreas: ["Uppder Chest", "Core", "Lower Chest"], description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?", exercises: exercises ?? [], restDays: 4)
+            let newTraining: Training = Training(name: "Test Trainig \(tIndex)", type: "Body Weight", duration: 45, difficulty: "Beginner", focusAreas: ["Uppder Chest", "Core", "Lower Chest"], description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?", exercises: exercises ?? [], restDays: 4) // TODO: Set max value count exercises to load only 4 for day.
             result.append(newTraining)
         }
         return result
